c14b78e16f6743ac7b3e51337bf46ee4510d28b7
worthr <worthr@4da98296-662a-0410-9f22-cfbba57dcc44>
2007-02-27 14:01:09 +0000

    add dasein package
    git-svn-id: https://patientview.svn.sourceforge.net/svnroot/patientview@13 4da98296-662a-0410-9f22-cfbba57dcc44

--- src/java/com/worthsoln/database/action/DatabaseAction.java
+++ src/java/com/worthsoln/database/action/DatabaseAction.java
@@ -2,11 +2,9 @@ package com.worthsoln.database.action;
 
 import javax.servlet.http.HttpServletRequest;
 import org.apache.struts.action.Action;
-import org.dasein.persist.MockSequencer;
 import org.dasein.persist.PersistenceException;
 import org.dasein.persist.Sequencer;
 import org.dasein.persist.SequencerInterface;
-import com.worthsoln.MockHolder;
 import com.worthsoln.database.DatabaseDAO;
 
 public abstract class DatabaseAction extends Action {
@@ -19,27 +17,27 @@ public abstract class DatabaseAction extends Action {
     public abstract String getDatabaseName();
 
     protected DatabaseDAO getDao(HttpServletRequest request) throws Exception {
-        if (request.getParameter("useMockDao") != null) {
-            DatabaseDAO mockDao = (DatabaseDAO) MockHolder.getInstance().getMock(getIdentifier());
-            if (mockDao == null) {
-                throw new Exception("MockHolder does not contain a mock called " + getIdentifier());
-            }
-            return mockDao;
-        }
+//        if (request.getParameter("useMockDao") != null) {
+//            DatabaseDAO mockDao = (DatabaseDAO) MockHolder.getInstance().getMock(getIdentifier());
+//            if (mockDao == null) {
+//                throw new Exception("MockHolder does not contain a mock called " + getIdentifier());
+//            }
+//            return mockDao;
+//        }
         return new DatabaseDAO(getDatabaseName());
     }
 
     private SequencerInterface getSequencer(HttpServletRequest request) {
-        if (request.getParameter("useMockDao") != null) {
-            return new MockSequencer();
-        }
+//        if (request.getParameter("useMockDao") != null) {
+//            return new MockSequencer();
+//        }
         return Sequencer.getInstance(getIdentifier(), getDatabaseName());
     }
 
     private SequencerInterface getSequencer(HttpServletRequest request, String identifier) {
-        if (request.getParameter("useMockDao") != null) {
-            return new MockSequencer();
-        }
+//        if (request.getParameter("useMockDao") != null) {
+//            return new MockSequencer();
+//        }
         return Sequencer.getInstance(identifier, getDatabaseName());
     }
 
