0832166015c2ff8e59c758234293f656fd2085b8
worthr <worthr@4da98296-662a-0410-9f22-cfbba57dcc44>
2009-03-04 01:44:32 +0000

    sort xml file into date order before loading
    git-svn-id: https://patientview.svn.sourceforge.net/svnroot/patientview@26 4da98296-662a-0410-9f22-cfbba57dcc44

--- src/java/com/worthsoln/patientview/FindXmlFiles.java
+++ src/java/com/worthsoln/patientview/FindXmlFiles.java
@@ -3,6 +3,7 @@ package com.worthsoln.patientview;
 import java.io.File;
 import java.io.FilenameFilter;
 import java.util.Arrays;
+import java.util.Comparator;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,9 +15,8 @@ import com.worthsoln.patientview.logon.LogonUtils;
 
 public class FindXmlFiles extends Action {
 
-    public ActionForward execute(
-            ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response)
-            throws Exception {
+    public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+                                 HttpServletResponse response) throws Exception {
         putXmlFilesInRequest(request);
         return LogonUtils.logonChecks(mapping, request);
     }
@@ -31,6 +31,11 @@ public class FindXmlFiles extends Action {
     public static File[] findXmlFiles(String xmlDirPath, String[] fileEndings) {
         File xmlDir = new File(xmlDirPath);
         File[] xmlFiles = xmlDir.listFiles(new XmlFileFilter(fileEndings));
+        Arrays.sort(xmlFiles, new Comparator<File>() {
+            public int compare(File f1, File f2) {
+                return Long.valueOf(f1.lastModified()).compareTo(f2.lastModified());
+            }
+        });
         return xmlFiles;
     }
 }
