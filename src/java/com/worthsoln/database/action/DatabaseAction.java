8300aa1b257a30185955fea15fbe1b22beedea4c
worthr <worthr@4da98296-662a-0410-9f22-cfbba57dcc44>
2009-11-19 15:12:26 +0000

    Remove no longer used Sequencer code
    git-svn-id: https://patientview.svn.sourceforge.net/svnroot/patientview@43 4da98296-662a-0410-9f22-cfbba57dcc44

--- src/java/com/worthsoln/database/action/DatabaseAction.java
+++ src/java/com/worthsoln/database/action/DatabaseAction.java
@@ -2,9 +2,6 @@ package com.worthsoln.database.action;
 
 import javax.servlet.http.HttpServletRequest;
 import org.apache.struts.action.Action;
-import org.dasein.persist.PersistenceException;
-import org.dasein.persist.Sequencer;
-import org.dasein.persist.SequencerInterface;
 import com.worthsoln.database.DatabaseDAO;
 
 public abstract class DatabaseAction extends Action {
@@ -17,47 +14,6 @@ public abstract class DatabaseAction extends Action {
     public abstract String getDatabaseName();
 
     protected DatabaseDAO getDao(HttpServletRequest request) throws Exception {
-//        if (request.getParameter("useMockDao") != null) {
-//            DatabaseDAO mockDao = (DatabaseDAO) MockHolder.getInstance().getMock(getIdentifier());
-//            if (mockDao == null) {
-//                throw new Exception("MockHolder does not contain a mock called " + getIdentifier());
-//            }
-//            return mockDao;
-//        }
         return new DatabaseDAO(getDatabaseName());
-    }
-
-    private SequencerInterface getSequencer(HttpServletRequest request) {
-//        if (request.getParameter("useMockDao") != null) {
-//            return new MockSequencer();
-//        }
-        return Sequencer.getInstance(getIdentifier(), getDatabaseName());
-    }
-
-    private SequencerInterface getSequencer(HttpServletRequest request, String identifier) {
-//        if (request.getParameter("useMockDao") != null) {
-//            return new MockSequencer();
-//        }
-        return Sequencer.getInstance(identifier, getDatabaseName());
-    }
-
-    protected int getNextInSequence(HttpServletRequest request) throws PersistenceException {
-        SequencerInterface sequencer = getSequencer(request);
-        long next = nonZeroSequence(sequencer);
-        return (new Long(next)).intValue();
-    }
-
-    protected int getNextInSequence(HttpServletRequest request, String identifier) throws PersistenceException {
-        SequencerInterface sequencer = getSequencer(request, identifier);
-        long next = nonZeroSequence(sequencer);
-        return (new Long(next)).intValue();
-    }
-
-    private long nonZeroSequence(SequencerInterface sequencer) throws PersistenceException {
-        long next = sequencer.next();
-        if (next == 0) {
-            next = sequencer.next();
-        }
-        return next;
     }
 }
--- src/java/org/dasein/persist/SequencerInterface.java
+++
@@ -1,6 +0,0 @@
-package org.dasein.persist;
-
-public interface SequencerInterface {
-
-    long next() throws PersistenceException;
-}
